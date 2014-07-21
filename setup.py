6f8b35cfdfe90a92c7d7a50628f243e73c24ff09
Yury Taranov <yury.taranov@activecloud.com>
2014-07-21 12:59:48 +0300

    Add dd wrapper

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from distutils.core import setup
 setup(
   name = 'lvsync',
   packages = ['lvsync'],
-  version = '201407.3',
+  version = '201407.4',
   description = 'Logical Volume Sync Tool',
   author = 'Yury Taranov',
   author_email = 'yury.taranov@gmail.com',
   url = 'https://github.com/yurytaranov/lvsync',
-  download_url = 'https://github.com/yurytaranov/lvsync/tarball/201407.3',
+  download_url = 'https://github.com/yurytaranov/lvsync/tarball/201407.4',
   keywords = ['lvm', 'sync', 'dd'],
   classifiers = [],
-  scripts=['scripts/lvsync']
+  scripts=[
+    'scripts/lvsync'
+    'scripts/dd-lvsync'
+  ]
 )
